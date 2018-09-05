3bf2927aa437924bdac72d8feddb6a278a86c94b
Sage Vogt <thurmanvogt@gmail.com>
2018-09-05 13:19:31 -0600

    Update scraper to direct to kingsoopers

--- script.js
+++ script.js
@@ -2,32 +2,45 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({show: true});
 
 nightmare
-  .goto('https://www.amazon.com')
-    .type('#twotabsearchtextbox', ['snickers'])
-    .click('input.nav-input')
-    .wait(1000)
-    .click('h2.a-size-base.s-inline.s-access-title.a-text-normal')
-    .wait(1000)
-    .click('input#add-to-cart-button')
-    .wait(1000)
-    .click('a#nav-cart')
-    .wait(1000)
-    .click('input.a-button-input')
-    .wait(1000)
-    .type('#ap_email', '[youremail'])
-    .wait(1000)
-    .click('input#continue')
-    .wait(1000)
-    .type('#ap_password', ['yourpassword'])
-    .wait(1000)
-    .click('input#signInSubmit')
-    .wait(4000)
-    .click('span#orderSummaryPrimaryActionBtn')
+  .goto('http://www.kingsoopers.com')
     .wait(3000)
-    .click('span#orderSummaryPrimaryActionBtn')
+    .click('a.Color.fo-TimeSlotButtonOptions-button.fo-FindStoreButton.Link')
+    .wait(1000)
+    .type('input.input-input.SearchBox-inputBox', '80202')
+    .wait(1000)
+    .click('button.SearchBox-searchButton')
     .wait(3000)
-    .click('span#submitOrderButtonId')
+    .click('button.SetPreferredStore-makePreferredLink')
     .wait(3000)
+    .type('input#searchbar.input-input.input', 'lamb')
+    .wait(1000)
+    .click('button.input-button')
+    .wait(3000)
+
+
+    //.click('input.a-button-input')
+    //.wait(1000)
+    //.type('#ap_email', '[youremail'])
+    //.wait(1000)
+   // .click('input#continue')
+//    .wait(1000)
+ //   .type('#ap_password', ['yourpassword'])
+ //   .wait(1000)
+//    .click('input#signInSubmit')
+//    .wait(4000)
+ //   .click('span#orderSummaryPrimaryActionBtn')
+ //   .wait(3000)
+//    .click('span#orderSummaryPrimaryActionBtn')
+//    .wait(3000)
+ //   .click('span#submitOrderButtonId')
+//    .wait(3000)
+//    .evaluate(function () {
+ //     var nameNodes = document.querySelectorAll('h3.thin.field-content')
+//      var list = [].slice.call(nameNodes); // Why did I have to do this?
+ //     return list.map(function(node){ 
+//        return node.innerText
+ //     });
+  //  })
   .end()
   .then(function (result) {
     console.log('you made food order');
