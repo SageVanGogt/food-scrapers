d20ea3023a8dbd687e2ee948ae5eb602b944c47e
Sage Vogt <thurmanvogt@gmail.com>
2018-09-05 15:16:39 -0600

    Add evaluation returning item name and price for safeway

--- script.js
+++ script.js
@@ -2,48 +2,29 @@ const Nightmare = require('nightmare');
 const nightmare = Nightmare({show: true});
 
 nightmare
-  .goto('http://www.kingsoopers.com')
+  .goto('https://shop.safeway.com/welcome.html')
     .wait(3000)
-    .click('a.Color.fo-TimeSlotButtonOptions-button.fo-FindStoreButton.Link')
+    .type('#zipcode', ['80202'])
     .wait(1000)
-    .type('input.input-input.SearchBox-inputBox', '80202')
+    .click('.btn.btn-default')
+    .wait(3000)
+    .type('#search-img', ['lamb'])
     .wait(1000)
-    .click('button.SearchBox-searchButton')
+    .click('.searchBtn')
     .wait(3000)
-    .click('button.SetPreferredStore-makePreferredLink')
-    .wait(3000)
-    .type('input#searchbar.input-input.input', 'lamb')
-    .wait(1000)
-    .click('button.input-button')
-    .wait(3000)
-
-
-    //.click('input.a-button-input')
-    //.wait(1000)
-    //.type('#ap_email', '[youremail'])
-    //.wait(1000)
-   // .click('input#continue')
-//    .wait(1000)
- //   .type('#ap_password', ['yourpassword'])
- //   .wait(1000)
-//    .click('input#signInSubmit')
-//    .wait(4000)
- //   .click('span#orderSummaryPrimaryActionBtn')
- //   .wait(3000)
-//    .click('span#orderSummaryPrimaryActionBtn')
-//    .wait(3000)
- //   .click('span#submitOrderButtonId')
-//    .wait(3000)
-//    .evaluate(function () {
- //     var nameNodes = document.querySelectorAll('h3.thin.field-content')
-//      var list = [].slice.call(nameNodes); // Why did I have to do this?
- //     return list.map(function(node){ 
-//        return node.innerText
- //     });
-  //  })
+    .evaluate(function () {
+      console.log('cat')
+      var nameNodes = document.querySelectorAll('.product-title')
+      var priceNodes = document.querySelectorAll('.product-price')
+      var nameList = [].slice.call(nameNodes); // Why did I have to do this?
+      var priceList = [].slice.call(priceNodes)
+      return nameList.map(function(node, index){ 
+        return node.innerText + ', ' + priceList[index].innerText
+      });
+    })
   .end()
   .then(function (result) {
-    console.log('you made food order');
+    console.log(result);
   })
   .catch(function (error) {
     console.error('Search failed: fawk',
